1c5881383e84dbae1d84a240e60dc4f1efb285dc
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2019-11-29 18:02:31 -0600

    tratando de arreglar tree
    NO JALA >.<

--- manual-hermeneutica-app/src/app/app.module.ts
+++ manual-hermeneutica-app/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { ContextoInternoComponent } from './areas/contexto-interno/contexto-interno.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { ContentSidenavComponent } from './content-sidenav/content-sidenav.component';
-import { MatTreeModule } from '@angular/material/tree';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { ContextoInternoComponent } from "./areas/contexto-interno/contexto-interno.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { ContentSidenavComponent } from "./content-sidenav/content-sidenav.component";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -22,9 +23,10 @@ import { MatTreeModule } from '@angular/material/tree';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatTreeModule
+    MatTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- manual-hermeneutica-app/src/app/content-sidenav/content-sidenav.component.ts
+++ manual-hermeneutica-app/src/app/content-sidenav/content-sidenav.component.ts
@@ -1,27 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import { MatTreeFlattener, MatTreeFlatDataSource } from '@angular/material/tree';
+import { Component, OnInit } from "@angular/core";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import {
+  MatTreeFlattener,
+  MatTreeFlatDataSource
+} from "@angular/material/tree";
 
-interface AreaNode {
+// interface AreaNode {
+//   name: string;
+//   children?: AreaNode[];
+// }
+
+// const TREE_DATA: AreaNode[] = [
+//   {
+//     name: 'CONTEXTO INTERNO',
+//     children: [
+//       { name: 'Desde el Libro Mismo' },
+//       { name: 'Divisiones de los Párrafos' },
+//       { name: 'Agrupación de los Párrafos' },
+//       { name: 'Elemento Existencial Humano' },
+//       { name: 'Identificación de la Cultura' },
+//       { name: 'Evaluación de la Cultura' },
+//       { name: 'Técnica de Análisis de los Problemas' }
+//     ]
+//   }
+
+// ]
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optiona list of children.
+ */
+interface FoodNode {
   name: string;
-  children?: AreaNode[];
+  children?: FoodNode[];
 }
 
-const TREE_DATA: AreaNode[] = [
+const TREE_DATA: FoodNode[] = [
   {
-    name: 'CONTEXTO INTERNO',
+    name: "Fruit",
+    children: [{ name: "Apple" }, { name: "Banana" }, { name: "Fruit loops" }]
+  },
+  {
+    name: "Vegetables",
     children: [
-      { name: 'Desde el Libro Mismo' },
-      { name: 'Divisiones de los Párrafos' },
-      { name: 'Agrupación de los Párrafos' },
-      { name: 'Elemento Existencial Humano' },
-      { name: 'Identificación de la Cultura' },
-      { name: 'Evaluación de la Cultura' },
-      { name: 'Técnica de Análisis de los Problemas' }
+      {
+        name: "Green",
+        children: [{ name: "Broccoli" }, { name: "Brussel sprouts" }]
+      },
+      {
+        name: "Orange",
+        children: [{ name: "Pumpkins" }, { name: "Carrots" }]
+      }
     ]
   }
-
-]
+];
 
 interface ExampleFlatNode {
   expandable: boolean;
@@ -30,24 +62,30 @@ interface ExampleFlatNode {
 }
 
 @Component({
-  selector: 'content-sidenav',
-  templateUrl: './content-sidenav.component.html',
-  styleUrls: ['./content-sidenav.component.css']
+  selector: "content-sidenav",
+  templateUrl: "./content-sidenav.component.html",
+  styleUrls: ["./content-sidenav.component.css"]
 })
 export class ContentSidenavComponent {
-  private _transformer = (node: AreaNode, level: number) => {
+  private _transformer = (node: FoodNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
       name: node.name,
-      level: level,
+      level: level
     };
-  }
+  };
 
   treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level, node => node.expandable);
+    node => node.level,
+    node => node.expandable
+  );
 
   treeFlattener = new MatTreeFlattener(
-    this._transformer, node => node.level, node => node.expandable, node => node.children);
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  );
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
@@ -56,5 +94,4 @@ export class ContentSidenavComponent {
   }
 
   hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-
 }

743c5ca2ea854a465bce9c67215b6d253cc58f1e
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2019-12-05 10:44:51 -0600

    prepando para el deployamento jeje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, ChangeDetectorRef } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'manual-hermeneutica-app';
+  title = "manual-hermeneutica-app";
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
+  opened = true;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private router: Router
+  ) {
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+    router.events.subscribe(val => {
+      if (this.mobileQuery.matches) {
+        console.log("Si debe cerrarse");
+        this.opened = false;
+      }
+    });
   }
 
   ngOnDestroy(): void {
